a0bcd37ce0fffd3176ce16af27311bb4e586cf6f
Dave Nunez <noonz2@icloud.com>
2021-05-01 01:32:18 -0400

    numChecker function

--- php/laracasts/functions.php
+++ php/laracasts/functions.php
@@ -12,3 +12,14 @@ function checkAge($age){
         return false;
     }
 }
+
+function numChecker(Array $numList,int $num){
+    $i = 0; // keep track of array index
+    foreach ($numList as $nums) {
+        if ($nums[0] <= $num && $nums[1] >= $num) { // compare each number per specifications
+            return $i;
+        }
+        $i++;
+    }
+    return 'Not Found in list';
+}
--- php/laracasts/index.php
+++ php/laracasts/index.php
@@ -12,10 +12,21 @@ require 'functions.php';
 // $animals = ['dog','cat','horse','lion'];
 // dd($animals);
 
-if (checkAge(21)){
-    echo "Access";
-} else {
-    echo "Denied";
-}
+// if (checkAge(21)){
+//     echo "Access";
+// } else {
+//     echo "Denied";
+// }
+
+$numberList = [
+    [0,10],
+    [11,50],
+    [51,100],
+    [100,200],
+    [200,500]
+];
+
+echo numChecker($numberList, 550);
+
 
 require 'index.view.php';
